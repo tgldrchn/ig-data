860d0c6fdb1a4e86ba3e204f526fbdfbf9639896
tgldrchn <tuguldur9190@gmail.com>
2022-11-29 19:17:11 +0800

    added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='navbar'> </div>
+      <div className='container'>
+        <div className='posts'></div>
+        <div className='profile'></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
